147fe12b8924902e676c84792e262a4e82cd4eaa
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 01:00:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(){
-  if (katzDeliLine === []){
+  if (katzDeliLine.length < 1){
     return "There is nobody waiting to be served!";
   } else {
     return "Currently serving " + katzDeliLine[0]+".";
