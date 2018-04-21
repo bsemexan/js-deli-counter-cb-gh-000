8e1a8fd0f96c2dd408dc2b48b2bf288bcfb34cc0
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 01:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function currentLine(line){
   if (line.length < 1){
     return "The line is currently empty.";
   }
-  for (let i = 0; i < katzDeli.length; i++)
-  return "The line is currently:"+ katzDeli.indexOf(line);
+  for (let i = 0; i < katzDeli.length; i++){
+    return "The line is currently:"+ katzDeli.indexOf(line);
+  }
 }
