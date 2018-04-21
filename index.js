4219bb4a38566511b8078f1890405306ba628330
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 01:35:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(line){
     return "The line is currently empty.";
   }
   for (let i = 0; i < line.length; i++){
-    katzDeli.push('(i + 1). line[i]');
+    katzDeli.push((i + 1). line[i]);
     return "The line is currently: " + katzDeli.join(', ');
   }
 }
