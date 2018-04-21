fa4bb3bc44a68a251445a9d90946ef3ebf84c354
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 00:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeli.push(name)
-  return "Welcome, " +  +". You are " + katzDeliLine + " in line.";
+  return "Welcome, " + name +". You are " + katzDeliLine + " in line.";
 }
 
 function nowServing(){
