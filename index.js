c667adcc77ac7e04c04b00bdf105e7534e9607dc
Berry  Semexan <bsemexan@gmail.com>
2018-04-21 01:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name +". You are number " + (katzDeliLine.indexOf(name) + 1) + " in line.";
 }
 
-function nowServing(){
+function nowServing(line){
   if (katzDeli.length < 1){
     return "There is nobody waiting to be served!";
   }
